ca424c58ecd21726d5549e30a55978b71edc2743
MaurOrtizz <mauortiztorres2004@gmail.com>
2024-03-20 18:50:45 -0600

    practica 7

--- miApp-2132830/src/app/app.module.ts
+++ miApp-2132830/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ListaAlumnosComponent } from './lista-alumnos/lista-alumnos.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ListaAlumnosComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
