17dcc0776eb962e2cfcc2fd19609466fc3d85dd0
MaurOrtizz <mauortiztorres2004@gmail.com>
2024-04-16 21:41:42 -0600

    Practica 9: Servicios en Angular

--- miApp-2132830/src/app/app.module.ts
+++ miApp-2132830/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ListaAlumnosComponent } from './lista-alumnos/lista-alumnos.component';
+import { ProductosComponent } from './productos/productos.component';
 
 @NgModule({
-  declarations: [AppComponent, ListaAlumnosComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent, ListaAlumnosComponent, ProductosComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
